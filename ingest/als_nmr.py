a304fbc57a895d9d66d805ff2d275a230c9bad65
Runbo Jiang <rjiang2@lbl.gov>
2024-04-25 11:18:07 -0700

    Add ingesting code

---
+++ ingest/als_nmr.py
@@ -0,0 +1,125 @@
+from datetime import datetime
+from pathlib import Path
+import logging
+from collections import OrderedDict
+from pathlib import Path
+from typing import List
+import pandas as pd
+
+from pyscicat.client import (
+    encode_thumbnail,
+    ScicatClient, 
+    get_file_size, 
+    get_file_mod_time
+)
+
+from pyscicat.model import (
+    Attachment,
+    OrigDatablock,
+    DataFile,
+    RawDataset,
+    DatasetType,
+    Ownable
+)
+
+from scicat_beamline.ingestors.common_ingestor_code import create_data_file, create_data_files_list
+from scicat_beamline.scicat_utils import encode_image_2_thumbnail
+from scicat_beamline.utils import Issue
+
+ingest_spec = "als_nmr"
+
+logger = logging.getLogger("scicat_ingest.NMR")
+
+global_keywords = ["NMR"] 
+
+# Note: update the scicat_metadata based on the data you will upload
+# scicat metadata is seperate from scientific_metadata (do not duplicate)
+# note that this code requires the csv file only have one line for header, and one line for values
+metadata_path = "/Users/runbojiang/Desktop/LinkML_NMR/my-nmr-metadata.csv"
+df = pd.read_csv(metadata_path, sep=',', header=None)
+header, values = df.iloc[0].tolist(), df.iloc[1].tolist()
+linkml_metadata = OrderedDict(zip(header, values))
+
+scicat_metadata = {
+        "owner": linkml_metadata["owner"],
+        "email": linkml_metadata["email"],
+        "instrument_name": linkml_metadata["instrument_name"],
+        "pi": linkml_metadata["principal_investigator"],
+        "proposal": linkml_metadata["proposal"],
+        "techniques": [linkml_metadata["instrument_name"]], 
+        "derived_techniques": [],
+    }
+
+def ingest(
+    scicat_client: ScicatClient,
+    username: str,
+    file_path: Path,
+    thumbnail_dir: Path,
+    issues: List[Issue],
+) -> str:
+    "Ingest a folder of 1d nmr files"
+
+    now_str = datetime.isoformat(datetime.now()) + "Z"
+    ownable = Ownable(
+        owner="MWET",
+        contactEmail="rjiang2@lbl.gov",
+        createdBy="runboj",
+        updatedBy="runboj",
+        updatedAt=now_str,
+        createdAt=now_str,
+        ownerGroup="MWET",
+        accessGroups=["MWET", "ingestor"],
+        instrumentGroup="instrument-default",
+    )
+
+    issues: List[Issue] = []
+
+    # read scientific_metadata from the csv file
+    # only one row of metadata as of this protype (Apr 16)
+    # TODO: how to do this for multiple csv files???
+    header, values = df.iloc[0].tolist()[5:], df.iloc[1].tolist()[5:]
+    scientific_metadata = OrderedDict(zip(header, values))
+    
+    description = file_path.stem.replace("_", " ")
+    filename = file_path.name
+
+    dataset = RawDataset(
+        owner="Leo Gordon",
+        contactEmail="leo@gmail.com",
+        creationLocation="ALS ?.?.?",
+        datasetName=filename,
+        type=DatasetType.raw,
+        instrumentId="6.3.2",
+        proposalId=scicat_metadata['proposal'],
+        dataFormat="ALS BCS",
+        principalInvestigator=scicat_metadata['pi'],
+        sourceFolder=file_path.parent.as_posix(),
+        scientificMetadata=scientific_metadata,
+        sampleId=scientific_metadata['sample'],
+        isPublished=False,
+        description=description,
+        keywords=["NMR", "1d"],
+        creationTime=scientific_metadata['acquisition_date'],
+        **ownable.dict(),
+    )
+    print(file_path)
+    dataset_id = scicat_client.upload_raw_dataset(dataset) #upload_raw_dataset
+    data_file = DataFile(path=str(file_path), 
+                         size=get_file_size(Path(file_path)),
+                         time=get_file_mod_time(Path(file_path)),
+                         type="RawDatasets",
+                         )
+
+    data_block = OrigDatablock(
+        size=get_file_size(file_path),
+        datasetId=dataset_id,
+        dataFileList=[data_file],
+        **ownable.dict(),
+    )
+    scicat_client.upload_datablock(data_block)
+
+    return dataset_id, issues
+
+
+def get_file_size(file_path: Path) -> int:
+    return file_path.lstat().st_size
---
+++ ingest/ingest_script.py
@@ -0,0 +1,130 @@
+import glob
+from dotenv import load_dotenv
+import os
+from ingest import ingest
+import pathlib
+load_dotenv()
+
+
+env_file_path = ".env"
+
+# Check if the file exists
+if os.path.exists(env_file_path):
+    # Open the file and read its contents
+    with open(env_file_path, "r") as file:
+        # Read all lines from the file
+        env_contents = file.readlines()
+        # Print each line
+        for line in env_contents:
+            print(line.strip())
+else:
+    print(f"The file '{env_file_path}' does not exist.")
+
+
+# Examples of the vars from the .env file
+#ROOT_FOLDER=/home/j/programming/work/October_data
+#SCICAT_URL=https://mwet.lbl.gov/api/v3
+#USERNAME=username
+#INGEST_USER=username
+#PASSWORD=password
+#INGEST_SPEC=als_11012_igor OR als_11012_scattering OR als_11012_nexafs
+
+ROOT_FOLDER = os.getenv("ROOT_FOLDER")
+SCICAT_URL = os.getenv("SCICAT_URL")
+USERNAME = os.getenv("USERNAME")
+INGEST_USER = os.getenv("INGEST_USER")
+PASSWORD = os.getenv("PASSWORD")
+INGEST_SPEC = os.getenv("INGEST_SPEC")
+DERIVED_FOLDER = os.getenv("DERIVED_FOLDER")
+
+assert type(ROOT_FOLDER) == str and len(ROOT_FOLDER) != 0 
+assert type(SCICAT_URL) == str and len(SCICAT_URL) != 0 
+assert type(USERNAME) == str and len(USERNAME) != 0 
+assert type(PASSWORD) == str and len(PASSWORD) != 0 
+assert type(INGEST_USER) == str and len(INGEST_USER) != 0 
+assert type(INGEST_SPEC) == str and len(INGEST_SPEC) != 0 
+
+is_derived_folder = False
+
+ROOT_FOLDER = pathlib.Path(ROOT_FOLDER)
+
+pattern = None
+ingestor_location = None
+override_iterator = False
+ingest_files_iter = []
+if INGEST_SPEC == "als_11012_igor":
+    pattern = f"{ROOT_FOLDER}/CCD/*/dat/"
+    ingestor_location = pathlib.Path(os.getcwd(), "scicat_beamline/ingestors/als_11012_igor.py")
+elif INGEST_SPEC == "als_11012_scattering":
+    pattern = f"{ROOT_FOLDER}/CCD/*/"
+    ingestor_location = pathlib.Path(os.getcwd(), "scicat_beamline/ingestors/als_11012_scattering.py")
+elif INGEST_SPEC == "als_11012_nexafs":
+    pattern = f"{ROOT_FOLDER}/Nexafs/*"
+    ingestor_location = pathlib.Path(os.getcwd(), "scicat_beamline/ingestors/nexafs.py")
+elif INGEST_SPEC == "nsls2_rsoxs_sst1":
+    pattern = f"{ROOT_FOLDER}/*/"
+    ingestor_location = pathlib.Path(os.getcwd(), "scicat_beamline/ingestors/nsls2_RSoXS.py")
+elif INGEST_SPEC == "nsls2_nexafs_sst1":
+    override_iterator = True
+    pattern = f"{ROOT_FOLDER}/*"
+    ingest_files_iter = glob.iglob(pattern)
+    ingest_files_arr = []
+    for file_str in ingest_files_iter:
+        if file_str.endswith(".log") or file_str.endswith(".csv") or file_str.endswith(".txt"):
+            continue
+        ingest_files_arr.append(file_str)
+    ingest_files_iter = ingest_files_arr
+    ingestor_location = pathlib.Path(os.getcwd(), "scicat_beamline/ingestors/nsls2_nexafs_sst1.py")
+elif INGEST_SPEC == "als733_saxs":
+    pattern = f"{ROOT_FOLDER}/*.txt"
+    override_iterator = True
+    ingest_files_iter = glob.iglob(pattern)
+    ingest_files_arr = []
+    for file_str in ingest_files_iter:
+        # Matt Landsman said not to include these in ingestion
+        if "autoexpose" in file_str or "beamstop_test" in file_str:
+            continue
+        ingest_files_arr.append(file_str)
+    ingest_files_iter = ingest_files_arr
+    ingestor_location = pathlib.Path(os.getcwd(), "scicat_beamline/ingestors/als_733_SAXS.py")
+elif INGEST_SPEC == "nsls2_trexs_smi":
+    pattern = f"{ROOT_FOLDER}/*/"
+    ingestor_location = pathlib.Path(os.getcwd(), "scicat_beamline/ingestors/nsls2_TREXS_smi.py")
+elif INGEST_SPEC == "polyfts_dscft":
+    pattern = f"{ROOT_FOLDER}/*/"
+    ingestor_location = pathlib.Path(os.getcwd(), "scicat_beamline/ingestors/phip_sim.py")
+elif INGEST_SPEC == "amd_gromacs":
+    pattern = f"{ROOT_FOLDER}/*/"
+    ingestor_location = pathlib.Path(os.getcwd(), "scicat_beamline/ingestors/amd_gromacs.py")
+elif INGEST_SPEC == "als_632_nexafs":
+    pattern = f"{ROOT_FOLDER}/*"
+    ingestor_location = pathlib.Path(os.getcwd(), "scicat_beamline/ingestors/als_632_nexafs.py")
+elif INGEST_SPEC == "733_gisaxs_bladecoating":
+    assert type(DERIVED_FOLDER) == str and len(DERIVED_FOLDER) != 0 
+    is_derived_folder = True
+    pattern = f"{ROOT_FOLDER}/*/"
+    ingestor_location = pathlib.Path(os.getcwd(), "scicat_beamline/ingestors/733_gisaxs_bladecoating.py")
+elif INGEST_SPEC == "SMI_gisaxs_bladecoating":
+    type(DERIVED_FOLDER) == str and len(DERIVED_FOLDER) != 0 
+    is_derived_folder = True
+    pattern = f"{ROOT_FOLDER}/*/"
+    ingestor_location = pathlib.Path(os.getcwd(), "scicat_beamline/ingestors/nsls_smi_bladecoating.py")
+elif INGEST_SPEC == "als_nmr":
+    print("ah")
+    pattern = f"{ROOT_FOLDER}/*.csv"
+    ingestor_location = pathlib.Path(os.getcwd(), "scicat_beamline/ingestors/als_nmr.py")
+
+else:
+    raise Exception("Environment variable 'INGEST_SPEC' is invalid.")
+
+if override_iterator is False:
+    ingest_files_iter = glob.iglob(pattern)
+
+for ingest_file_str in ingest_files_iter:
+    ingest_file_path = pathlib.Path(ingest_file_str)
+    if ingest_file_path.exists():
+        print(ingest_file_path)
+        if is_derived_folder is False:
+            ingest(ingestor_location, ingest_file_path, None,  INGEST_USER, SCICAT_URL, token=None, username=USERNAME, password=PASSWORD)
+        else:
+            ingest(ingestor_location, ingest_file_path, pathlib.Path(DERIVED_FOLDER), INGEST_USER, SCICAT_URL, token=None, username=USERNAME, password=PASSWORD)

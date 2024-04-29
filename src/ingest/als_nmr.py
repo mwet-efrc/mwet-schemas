cf302ec50583b6a89b5c106acf000bc5aa551adb
Runbo Jiang <rjiang2@lbl.gov>
2024-04-29 13:31:57 -0700

    Update metadata_path

--- src/ingest/als_nmr.py
+++ src/ingest/als_nmr.py
@@ -15,16 +15,16 @@ from pyscicat.client import (
 
 from pyscicat.model import (
     Attachment,
-    OrigDatablock,
+    CreateDatasetOrigDatablockDto,
     DataFile,
     RawDataset,
     DatasetType,
     Ownable
 )
 
-from scicat_beamline.ingestors.common_ingestor_code import create_data_file, create_data_files_list
-from scicat_beamline.scicat_utils import encode_image_2_thumbnail
-from scicat_beamline.utils import Issue
+# from scicat_beamline.ingestors.common_ingestor_code import create_data_file, create_data_files_list
+# from scicat_beamline.scicat_utils import encode_image_2_thumbnail
+# from scicat_beamline.utils import Issue
 
 ingest_spec = "als_nmr"
 
@@ -35,7 +35,7 @@ global_keywords = ["NMR"]
 # Note: update the scicat_metadata based on the data you will upload
 # scicat metadata is seperate from scientific_metadata (do not duplicate)
 # note that this code requires the csv file only have one line for header, and one line for values
-metadata_path = "/Users/runbojiang/Desktop/LinkML_NMR/my-nmr-metadata.csv"
+metadata_path = "/Users/runbojiang/Desktop/LinkML_NMR/src/example_data/example-nmr-metadata.csv"
 df = pd.read_csv(metadata_path, sep=',', header=None)
 header, values = df.iloc[0].tolist(), df.iloc[1].tolist()
 linkml_metadata = OrderedDict(zip(header, values))
@@ -55,7 +55,7 @@ def ingest(
     username: str,
     file_path: Path,
     thumbnail_dir: Path,
-    issues: List[Issue],
+    issues,
 ) -> str:
     "Ingest a folder of 1d nmr files"
 
@@ -72,7 +72,8 @@ def ingest(
         instrumentGroup="instrument-default",
     )
 
-    issues: List[Issue] = []
+    # issues: List[Issue] = []
+    issues = []
 
     # read scientific_metadata from the csv file
     # only one row of metadata as of this protype (Apr 16)
@@ -103,20 +104,22 @@ def ingest(
         **ownable.dict(),
     )
     print(file_path)
-    dataset_id = scicat_client.upload_raw_dataset(dataset) #upload_raw_dataset
-    data_file = DataFile(path=str(file_path), 
+    file_name = Path(file_path).name
+    print(file_name)
+    dataset_id = scicat_client.upload_new_dataset(dataset)
+    data_file = DataFile(path=str(file_name), 
                          size=get_file_size(Path(file_path)),
                          time=get_file_mod_time(Path(file_path)),
                          type="RawDatasets",
                          )
 
-    data_block = OrigDatablock(
+    data_block = CreateDatasetOrigDatablockDto(
         size=get_file_size(file_path),
         datasetId=dataset_id,
         dataFileList=[data_file],
         **ownable.dict(),
     )
-    scicat_client.upload_datablock(data_block)
+    scicat_client.datasets_origdatablock_create(dataset_id, data_block)
 
     return dataset_id, issues
 
